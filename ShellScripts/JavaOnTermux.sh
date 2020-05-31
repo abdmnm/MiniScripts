06a777fce493e88287055cd0d16b3e5c966573dc
Abm <abdullah.rcpsc@gmail.com>
2020-05-31 23:31:04 +0600

    Deal with possible compilation errors
    Script should now exit if ecj or dx fails. It should be checked whether
    or not the compiler messages are affected.

--- ShellScripts/JavaOnTermux.sh
+++ ShellScripts/JavaOnTermux.sh
@@ -9,14 +9,32 @@ then
 	file_name=$(echo "$file_name" | cut -f 1 -d '.')
 
 	# Compile and Execute
+	echo ecj :
 	ecj $file_name.java
-	dx --dex --output=$file_name.dex $file_name.class
-	dalvikvm -cp $file_name.dex $file_name ${@:2}
+	if [ $? -eq 0 ]
+	then
+		echo dx :
+		dx --dex --output=$file_name.dex $file_name.class
+	else
+		echo ecj failure
+		exit 1
+	fi
+	if [ $? -eq 0 ]
+	then
+		echo dalvikvm :
+		dalvikvm -cp $file_name.dex $file_name ${@:2}
+	else 
+		echo dx failure
+		exit 1
+	fi
+	if [ $? -eq 1 ]
+	then
+		echo dalvikvm failure
+	fi
 
 elif [ "$cmd_arg" == "install" ]
 then
-	apt update
-	apt install ecj4.6 dx
+	apt install ecj dx
 else
 	echo "Script to run Java on Android in Termux"
 	echo "usage: bash JavaOnTermux.sh <option | source file>"
