e02f568030d17a142327e60b85726ffdf2442d21
abdmnm <60595752+abdmnm@users.noreply.github.com>
2020-06-08 12:29:05 +0000

    Update JavaOnTermux.sh

--- ShellScripts/JavaOnTermux.sh
+++ ShellScripts/JavaOnTermux.sh
@@ -10,13 +10,28 @@ then
 
 	# Compile and Execute
 	ecj $file_name.java
-	dx --dex --output=$file_name.dex $file_name.class
-	dalvikvm -cp $file_name.dex $file_name ${@:2}
+	if [ $? -eq 0 ]
+	then
+		dx --dex --output=$file_name.dex $file_name.class
+	else
+		echo ecj failure
+		exit 1
+	fi
+	if [ $? -eq 0 ]
+	then
+		dalvikvm -cp $file_name.dex $file_name ${@:2}
+	else 
+		echo dx failure
+		exit 1
+	fi
+	if [ $? -eq 1 ]
+	then
+		echo dalvikvm failure
+	fi
 
 elif [ "$cmd_arg" == "install" ]
 then
-	apt update
-	apt install ecj4.6 dx
+	apt install ecj dx
 else
 	echo "Script to run Java on Android in Termux"
 	echo "usage: bash JavaOnTermux.sh <option | source file>"
